10b768c5bd8ec6dac39859cc124ec854d9a75ee4
Divyansh-Gupta-Antier <155991189+Divyansh-Gupta-Antier@users.noreply.github.com>
2025-09-26 18:33:13 +0530

    Fix default percentage split and incorrect error when buying exact tokens (#24)

--- src/chaincode/launchpad/buyExactToken.ts
+++ src/chaincode/launchpad/buyExactToken.ts
@@ -89,7 +89,7 @@ export async function buyExactToken(
   // Transfer transaction fees
   const launchpadFeeAddressConfiguration = await fetchLaunchpadFeeAddress(ctx);
   if (launchpadFeeAddressConfiguration && transactionFees) {
-    const totalRequired = new BigNumber(buyTokenDTO.tokenQuantity).plus(transactionFees);
+    const totalRequired = nativeTokensToBuy.plus(new BigNumber(transactionFees));
 
     const buyerBalance = await fetchOrCreateBalance(ctx, ctx.callingUser, sale.nativeToken);
     if (buyerBalance.getQuantityTotal().lt(totalRequired)) {
